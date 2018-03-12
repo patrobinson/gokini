d663399b52c49afa7bf6668268c2dcbdb6418c2e
Patrick Robinson <patrick.robinson@envato.com>
2018-03-12 18:17:11 +1100

    Add integration test for gaining an expired lease

--- integration_test.go
+++ integration_test.go
@@ -38,6 +38,7 @@ func TestCheckpointRecovery(t *testing.T) {
 		RecordConsumer:       rc,
 		TableName:            "checkpoint_recovery",
 		EmptyRecordBackoffMs: 2000,
+		LeaseDuration:        1,
 	}
 	pushRecordToKinesis("checkpoint_recovery", []byte("abcd"))
 
@@ -66,6 +67,51 @@ func TestCheckpointRecovery(t *testing.T) {
 		fmt.Printf("seqenceID: %s, processed %d time(s)\n", sequenceID, timesSequenceProcessed)
 		if timesSequenceProcessed > 1 {
 			t.Errorf("Sequence number %s was processed more than once", sequenceID)
+		}
+	}
+	kc.Shutdown()
+}
+
+func TestCheckpointGainLock(t *testing.T) {
+	rc := &IntegrationRecordConsumer{
+		processedRecords: make(map[string]int),
+	}
+	kc := &KinesisConsumer{
+		StreamName:           "checkpoint_gain_lock",
+		ShardIteratorType:    "TRIM_HORIZON",
+		RecordConsumer:       rc,
+		TableName:            "checkpoint_gain_lock",
+		EmptyRecordBackoffMs: 2000,
+		LeaseDuration:        1,
+	}
+	pushRecordToKinesis("checkpoint_gain_lock", []byte("abcd"))
+
+	err := kc.StartConsumer()
+	if err != nil {
+		t.Errorf("Error starting consumer %s", err)
+	}
+
+	kc.Shutdown()
+	time.Sleep(1 * time.Second)
+
+	kc = &KinesisConsumer{
+		StreamName:        "checkpoint_gain_lock",
+		ShardIteratorType: "TRIM_HORIZON",
+		RecordConsumer:    rc,
+		TableName:         "checkpoint_gain_lock",
+		LeaseDuration:     1,
+	}
+
+	err = kc.StartConsumer()
+	if err != nil {
+		t.Errorf("Error starting consumer %s", err)
+	}
+	pushRecordToKinesis("checkpoint_gain_lock", []byte("abcd"))
+	time.Sleep(1 * time.Second)
+	if len(rc.processedRecords) != 2 {
+		t.Errorf("Expected to have processed 2 records")
+		for sequenceId, timesProcessed := range rc.processedRecords {
+			fmt.Println("Processed", sequenceId, timesProcessed, "time(s)")
 		}
 	}
 	kc.Shutdown()
