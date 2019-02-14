c92954ea07bd6d3ac4a911d2de59fdaf448c9785
Patrick Robinson <patrick.robinson@envato.com>
2019-02-15 10:27:33 +1100

    Add failing tests to exhibit race condition

--- checkpointer_test.go
+++ checkpointer_test.go
@@ -96,20 +96,94 @@ func TestGetLeaseAquired(t *testing.T) {
 		"LeaseTimeout": {
 			S: aws.String(time.Now().AddDate(0, -1, 0).UTC().Format(time.RFC3339)),
 		},
+		"SequenceID": {
+			S: aws.String("deadbeef"),
+		},
 	}
 	input := &dynamodb.PutItemInput{
 		TableName: aws.String("TableName"),
 		Item:      marshalledCheckpoint,
 	}
 	checkpoint.svc.PutItem(input)
-	err := checkpoint.GetLease(&shardStatus{
+	shard := &shardStatus{
 		ID:         "0001",
 		Checkpoint: "",
 		mux:        &sync.Mutex{},
-	}, "ijkl-mnop")
+	}
+	err := checkpoint.GetLease(shard, "ijkl-mnop")
 
 	if err != nil {
 		t.Errorf("Lease not aquired after timeout %s", err)
+	}
+}
+
+func TestRaceCondGetLeaseTimeout(t *testing.T) {
+	svc := &mockDynamoDB{tableExist: true}
+	checkpoint := &DynamoCheckpoint{
+		TableName: "TableName",
+	}
+	checkpoint.Init()
+	checkpoint.svc = svc
+	marshalledCheckpoint := map[string]*dynamodb.AttributeValue{
+		"ShardID": {
+			S: aws.String("0001"),
+		},
+		"AssignedTo": {
+			S: aws.String("abcd-efgh"),
+		},
+		"LeaseTimeout": {
+			S: aws.String(time.Now().AddDate(0, -1, 0).UTC().Format(time.RFC3339)),
+		},
+		"SequenceID": {
+			S: aws.String("deadbeef"),
+		},
+	}
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String("TableName"),
+		Item:      marshalledCheckpoint,
+	}
+	checkpoint.svc.PutItem(input)
+	shard := &shardStatus{
+		ID:         "0001",
+		Checkpoint: "",
+		mux:        &sync.Mutex{},
+	}
+	err := checkpoint.GetLease(shard, "ijkl-mnop")
+
+	if err == nil || err.Error() != ErrLeaseNotAquired {
+		t.Error("Got a lease when checkpoints didn't match. Potentially we stomped on the checkpoint")
+	}
+}
+
+func TestRaceCondGetLeaseNoAssignee(t *testing.T) {
+	svc := &mockDynamoDB{tableExist: true}
+	checkpoint := &DynamoCheckpoint{
+		TableName: "TableName",
+	}
+	checkpoint.Init()
+	checkpoint.svc = svc
+	marshalledCheckpoint := map[string]*dynamodb.AttributeValue{
+		"ShardID": {
+			S: aws.String("0001"),
+		},
+		"SequenceID": {
+			S: aws.String("deadbeef"),
+		},
+	}
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String("TableName"),
+		Item:      marshalledCheckpoint,
+	}
+	checkpoint.svc.PutItem(input)
+	shard := &shardStatus{
+		ID:         "0001",
+		Checkpoint: "",
+		mux:        &sync.Mutex{},
+	}
+	err := checkpoint.GetLease(shard, "ijkl-mnop")
+
+	if err == nil || err.Error() != ErrLeaseNotAquired {
+		t.Error("Got a lease when checkpoints didn't match. Potentially we stomped on the checkpoint")
 	}
 }
 
